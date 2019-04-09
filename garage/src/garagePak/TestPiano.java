3cc8f27ad0bc371a39117bf493731876a450f463
Cristian <hp@DESKTOP-ETSKT4S>
2019-04-09 15:22:20 +0200

    Classe Piano Funzionante

---
+++ garage/src/garagePak/TestPiano.java
@@ -0,0 +1,18 @@
+package garagePak;
+
+
+public class TestPiano {
+	public static void main ( String [] args ) {
+	
+		Piano p1 = new Piano(10,10,10,1);
+		p1.prenotaPosto("Van");
+		p1.prenotaAbbonamento("Lusso");
+		p1.getPiano();
+		p1.getPostiOccupati("Van");
+		p1.tempoPermanenza(2);
+		p1.liberaPosto(2);
+		p1.verficaAbbonato("Lusso");
+		
+		
+		}
+}
